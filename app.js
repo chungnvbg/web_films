125f6fbf0d807a73174d45eca51392f594225d50
namtdn <namtd@sphinx-software.com>
2019-05-13 17:15:56 +0700

    nam commit this file

---
+++ app.js
@@ -0,0 +1,36 @@
+require('dotenv').config();
+const Koa             = require('koa');
+const config          = require('./knexfile');
+const knex            = require('knex')(config.development);
+const nunjuck         = require('./nunjucks.provider');
+const routerLogin     = require('./router/RouterLogin');
+const serve           = require('koa-static');
+const path            = require('path');
+const bodyParser      = require('koa-bodyparser');
+const session         = require('koa-session');
+
+const authProvider    = require('./auth/Auth.Provider');
+const userProvider    = require('./user/UserProvider');
+const hasherProvider  = require('./hasher/hasherProvider');
+const routerDashboard = require('./router/RouteDashboard');
+const staticPath      = '/views';
+
+const app = new Koa();
+
+app.keys = ['some-secret-key'];
+
+app.use(serve(
+    path.join(__dirname, staticPath)
+));
+app.use(session(app));
+app.use(hasherProvider(10));
+app.use(bodyParser());
+app.use(nunjuck());
+app.use(userProvider(knex));
+app.use(authProvider());
+app.use(routerDashboard.routes());
+app.use(routerLogin.routes());
+
+app.listen(process.env.PORT, () => {
+    console.log('Server listen port 6060')
+});
